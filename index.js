6fe44e5ec1ace9ce1e6ef50ae1a4e834443406f7
Rickyslash <85386584+ricky-kiva@users.noreply.github.com>
2022-11-20 17:00:04 +0000

    add res.json by POST on /fileanalyse API

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer')
+var storage = multer.memoryStorage(); 
+const upload = multer({ storage: storage })
 require('dotenv').config()
 
 var app = express();
@@ -11,8 +14,13 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), function(req,res) {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
